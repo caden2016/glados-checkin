90a1b77f6250200ec6ad429a0b3b07fae8f9ddb5
caden <gqx@qq.com>
2023-02-22 23:05:54 +0800

    c1

--- checkin.py
+++ checkin.py
@@ -8,7 +8,7 @@ sckey = os.environ["SCKEY"]
 # 填入glados账号对应cookie
 cookie = os.environ["COOKIE"]
 #'__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
-
+_gid=GA1.2.1481544963.1677068957; koa:sess=eyJ1c2VySWQiOjI4MTM3MSwiX2V4cGlyZSI6MTcwMjk4OTM1NjEyNSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=0kw83ZeV0YM6G6I_pz9ekhfjkac; __stripe_mid=b029a063-fd51-46f1-b1e3-7d7ee32223ec62e1f9; Cookie=enabled; Cookie.sig=lbtpENsrE0x6riM8PFTvoh9nepc; _gat_gtag_UA_104464600_2=1; _ga_CZFVKMNT9J=GS1.1.1677077104.2.1.1677077116.0.0.0; _ga=GA1.1.1992516428.1677068957
 
 
 def start():
@@ -16,7 +16,7 @@ def start():
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer },body={"token":"glados.network"})
     state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
    # print(res)
 
